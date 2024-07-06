90628de3d91add50ce9fff32ed9e98488d6f538c
nullh <nullh@gmail.com>
2024-07-06 09:38:27 +0700

    [ADD] first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		message := os.Getenv("HELLO_MESSAGE")
+		if message == "" {
+			message = "Hello, World!"
+		}
+		return c.SendString(message)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Fatal(app.Listen(":" + port))
+}
